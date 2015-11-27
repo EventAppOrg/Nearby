c77efa22e8cf7ad27fd6244f931214917407d922
Kavin Arasu <kavin@groupon.com>
2015-11-26 17:33:23 -0800

    Add basic detail view

---
+++ Nearby/EventDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailViewController.h
+//  Nearby
+//
+//  Created by Kavin Arasu on 11/26/15.
+//  Copyright © 2015 EventAppOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailViewController : UIViewController
+
+@end
